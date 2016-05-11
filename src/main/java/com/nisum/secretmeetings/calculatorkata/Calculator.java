239eeaffd39f7c9cf196b588e48d2c0e2acc7015
German Gonzalez <German_Gonzalez@gap.com>
2016-05-11 11:47:58 -0300

    Test para suma de varios números

--- src/main/java/com/nisum/secretmeetings/calculatorkata/Calculator.java
+++ src/main/java/com/nisum/secretmeetings/calculatorkata/Calculator.java
@@ -1,8 +1,24 @@
 package com.nisum.secretmeetings.calculatorkata;
 
+
+import static java.lang.Integer.parseInt;
+
+
 public class Calculator {
 
-    public int calculate (String input) {
-        return Integer.parseInt(input);
+    public int calculate(String input) {
+        if (input.contains("+")) {
+            String[] numbers = input.split("\\+");
+
+            int total = 0;
+
+            for (String numberAsString : numbers) {
+                total += parseInt(numberAsString);
+            }
+
+            return total;
+        } else {
+            return parseInt(input);
+        }
     }
 }
--- src/test/java/com/nisum/secretmeetings/calculatorkata/CalculatorTest.java
+++ src/test/java/com/nisum/secretmeetings/calculatorkata/CalculatorTest.java
@@ -1,23 +1,65 @@
 package com.nisum.secretmeetings.calculatorkata;
 
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class CalculatorTest {
 
-    @Test
-    public void shouldCreateCalculatorInstance(){
-        Calculator calculator = new Calculator();
-        assertNotNull(calculator);
+    private Calculator calculator;
+
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
     }
 
+
     @Test
-    public void shouldReturnTheSameNumberOnSingleNumberInput(){
-        Calculator calculator = new Calculator();
+    public void shouldReturnTheSameNumberOnSingleNumberInput() throws Exception {
         assertEquals(7, calculator.calculate("7"));
         assertEquals(8, calculator.calculate("8"));
+        assertEquals(-42, calculator.calculate("-42"));
+        assertEquals(Integer.MAX_VALUE, calculator.calculate("2147483647"));
+        assertEquals(Integer.MIN_VALUE, calculator.calculate("-2147483648"));
     }
 
+
+    @Test
+    public void shouldSumTwoNumbers() throws Exception {
+        assertThat(calculator.calculate("1+2"), is(3));
+        assertThat(calculator.calculate("2+2"), is(4));
+        assertThat(calculator.calculate("-2+-2"), is(-4));
+    }
+
+
+    @Test
+    public void shouldSumThreeNumbers() throws Exception {
+        assertThat(calculator.calculate("2+2+2"), is(6));
+        assertThat(calculator.calculate("-2+-2+-2"), is(-6));
+    }
+
+
+    @Test
+    public void shouldSumFourNumbers() throws Exception {
+        assertThat(calculator.calculate("1+2+3+4"), is(10));
+        assertThat(calculator.calculate("-1+-2+-3+-4"), is(-10));
+    }
+
+    //TODO restar dos números
 }
+
+
+
+
+
+
+
+
+
+
